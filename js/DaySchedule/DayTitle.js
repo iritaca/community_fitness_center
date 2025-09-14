af28c7c7c8b71c9eec164eacc0ac0a6c2d797017
Isaac Rios <jirb1213@live.com.mx>
2025-09-14 17:48:56 -0600

    schedule day title

---
+++ js/DaySchedule/DayTitle.js
@@ -0,0 +1,15 @@
+// @TODO: Create the DayAccordionTitle, and add proper styles
+export class DayTitle{
+    constructor({title}){
+        this.title = title
+
+        this.container = document.createElement('div')
+        this.container.classList.add('schedule-header')
+
+        this.titleEl= document.createElement('h3')
+        this.titleEl.textContent=this.title
+
+        this.container.appendChild(this.titleEl)
+
+    }
+}
