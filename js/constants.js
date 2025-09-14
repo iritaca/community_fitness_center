2fd8ca56899bb00466ebd8e858f31ed80cd35194
Isaac Rios <jirb1213@live.com.mx>
2025-09-14 17:45:04 -0600

    constants file created and minor html id update

---
+++ js/constants.js
@@ -0,0 +1,15 @@
+export const ACTIVITIES_BY_DAY=[
+    {day:'monday',activities:[
+        {activity:'strength',instructor:'rochelle',time:'5:00am'},
+        {activity:'pilates',instructor:'stephanie',time:'8:15am'},
+        {activity:'weights',instructor:'jen',time:'9:15am'},
+        {activity:'kickboxing',instructor:'barbie',time:'10:15am'},
+        {activity:'strength_&_flexibility',instructor:'stephanie',time:'11:30am'},
+        {activity:'all-ages_endurance',instructor:'stephanie',time:'12:30pm'},
+        {activity:'weights',instructor:'lisa',time:'05:00pm'},
+        {activity:'yoga_flow',instructor:'mandy',time:'06:00pm'},
+        {activity:'all-american_dance',instructor:'mandy',time:'07:00pm'},
+    ]}
+]
+
+export const ACTIVITIES =['strength','pilates','weights','kickboxing','strength & flexibility','all-ages endurance','yoga & flow','all-american dance','ultimate endurance','skinny fit','fit & flex','fitness mix']
