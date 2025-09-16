f4cb8909069b0653e54d59b3950f6e7e346a7d0d
Isaac Rios <jirb1213@live.com.mx>
2025-09-16 16:24:50 -0600

    scheduleList: getPresentDay to get current day and time

--- js/DaySchedule/Schedule.js
+++ js/DaySchedule/Schedule.js
@@ -1,4 +1,5 @@
 
+import { Observable } from '../Observable.js';
 import { ValidateWrapper } from '../utils.js';
 import { ActivityCardList } from './ActivityCard.js';
 import { DayTitle } from './DayTitle.js';
@@ -9,8 +10,9 @@ import { DayTitle } from './DayTitle.js';
 const VALID_DAYS=['monday','tuesday','wednesday','thursday','friday']
 
 export class Day{
-    constructor({day}){
+    constructor({day,time}){
         this.day = day
+        this.time= time
         this.container = document.createElement('div')
         
         console.log(this.day)
@@ -19,26 +21,47 @@ export class Day{
     render(){
         let headerToRender =  new DayTitle({title:this.day})
         this.container.appendChild(headerToRender.container)
-        const cardList = new ActivityCardList(this.day)
+        const cardList = new ActivityCardList({day:this.day,time:this.time})
         this.container.appendChild(cardList.container)
         
     }
 }
 
-export class ScheduleList{
-    constructor({wrapper,type='weekly'}){
+export class ScheduleList extends Observable{
+    constructor({wrapper,type='daily'}){
+        super()
         this.wrapper = new ValidateWrapper({wrapper,componentName:this.constructor.name}).getWrapper()
         this.type = type
-
         this.render()
     }
+
+    setType(type){
+        this.type = type
+        this.render()
+    }
+
+    getPresentDay(){
+        const now = new Date()
+
+        const day = new Intl.DateTimeFormat('en-US',{weekday:'long'}).format(now).toLowerCase()
+
+        const time = now.toLocaleTimeString('en-US',{
+            hour:'2-digit',
+            minute:'2-digit',
+            hour12:false
+        })
+
+        return {day,time}
+    }
+
 
     render(){
         this.wrapper.innerHTML = ''
 
         if(this.type==='daily'){
             //return present day
-            const today = new Day({day:'monday'})
+            const present = this.getPresentDay()
+            const today = new Day({day:present.day,time:present.time})
             this.wrapper.appendChild(today.container)
             return 
             // return selected day from date input
