9d3a8c82e9f556343a0d98e0adb38843e13d4b74
Isaac Rios <jirb1213@live.com.mx>
2025-09-16 16:23:37 -0600

    activity card list order by time and additional style added by comparing present time

--- js/DaySchedule/ActivityCard.js
+++ js/DaySchedule/ActivityCard.js
@@ -21,7 +21,7 @@ class ActivityCard{
 
         this.timeEl=document.createElement('span')
         this.timeEl.classList.add('activity-card-time')
-        this.timeEl.textContent=this.time
+        this.timeEl.textContent=this.time + ' hr'
 
         this.container.appendChild(this.titleEl)
         this.container.appendChild(this.teacherEl)
@@ -32,20 +32,25 @@ class ActivityCard{
 }
 
 export class ActivityCardList{
-    constructor(day){
-        this.day = day        
+    constructor({day,time}){
+        this.day = day
+        this.time = time
         this.activities=ACTIVITIES_BY_DAY.find(date=>date.day===this.day).activities
         this.container=document.createElement('ul')
         this.render()
     }
-
+    
     render(){
         const frag = document.createDocumentFragment()
         this.container.classList.add('activity-card-list')
 
-        for(const activity of this.activities){
+        const activitiviesSortedByTime = [...this.activities].sort((a,b)=> a.time.localeCompare(b.time))
+        
+        for(const activity of activitiviesSortedByTime){
             const liEl =document.createElement('li')
             liEl.classList.add('activity-card-item')
+            if(this.time>=activity.time) liEl.classList.add('isOutOfTime')
+            console.log({time:activity.time})
             liEl.appendChild(new ActivityCard({activity}).container)
             frag.appendChild(liEl)
         }
