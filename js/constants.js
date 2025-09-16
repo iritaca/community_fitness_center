bd10453eebcd062c144b5ebac1cd0ebb3ec1e9ce
Isaac Rios <jirb1213@live.com.mx>
2025-09-15 22:49:24 -0600

    constants: footer links and activities object

--- js/constants.js
+++ js/constants.js
@@ -1,15 +1,57 @@
+// @TODO: The instructors could have real names, this information could be taken from an API
 export const ACTIVITIES_BY_DAY=[
     {day:'monday',activities:[
-        {activity:'strength',instructor:'rochelle',time:'5:00am'},
-        {activity:'pilates',instructor:'stephanie',time:'8:15am'},
-        {activity:'weights',instructor:'jen',time:'9:15am'},
+        {activity:'strength',instructor:'rochelle',time:'05:00am'},
+        {activity:'pilates',instructor:'stephanie',time:'08:15am'},
+        {activity:'weights',instructor:'jen',time:'09:15am'},
         {activity:'kickboxing',instructor:'barbie',time:'10:15am'},
-        {activity:'strength_&_flexibility',instructor:'stephanie',time:'11:30am'},
-        {activity:'all-ages_endurance',instructor:'stephanie',time:'12:30pm'},
+        {activity:'strength & flexibility',instructor:'stephanie',time:'11:30am'},
+        {activity:'all-ages endurance',instructor:'stephanie',time:'12:30pm'},
         {activity:'weights',instructor:'lisa',time:'05:00pm'},
-        {activity:'yoga_flow',instructor:'mandy',time:'06:00pm'},
-        {activity:'all-american_dance',instructor:'mandy',time:'07:00pm'},
-    ]}
+        {activity:'yoga flow',instructor:'mandy',time:'06:00pm'},
+        {activity:'all-american dance',instructor:'mandy',time:'07:00pm'},
+    ]},
+    {day:'tuesday',activities:[
+        {activity:'strength',instructor:'barbie',time:'06:00am'},
+        {activity:'ultimate endurance',instructor:'mandy',time:'09:15am'},
+        {activity:'yoga & flow',instructor:'elisa',time:'09:45am'},
+        {activity:'skinny fit',instructor:'stephanie',time:'05:30pm'},
+        {activity:'ultimate endurance',instructor:'fin',time:'06:30pm'},
+    ]},
+    {day:'wednesday',activities:[
+        {activity:'strength',instructor:'rochelle',time:'05:00am'},
+        {activity:'all-american dance',instructor:'mandy',time:'09:15am'},
+        {activity:'strength',instructor:'lisa',time:'10:15am'},
+        {activity:'fit & flex',instructor:'stephanie',time:'11:30am'},
+        {activity:'strength & flexibility',instructor:'stephanie',time:'12:30pm'},
+        {activity:'weights',instructor:'susanne',time:'05:00pm'},
+        {activity:'yoga & flow',instructor:'barbie',time:'06:00pm'},
+        {activity:'all-american dance',instructor:'ali',time:'07:00pm'},
+    ]},
+    {day:'thursday',activities:[
+        {activity:'strength',instructor:'barbie',time:'06:00am'},
+        {activity:'yoga & flow',instructor:'elisa',time:'09:15am'},
+        {activity:'kickboxing',instructor:'jen',time:'09:45am'},
+        {activity:'all-ages endurance',instructor:'stephanie',time:'11:30am'},
+        {activity:'ultimate endurance',instructor:'jen',time:'06:30pm'},
+        {activity:'fitness mix',instructor:'suzanne',time:'07:00pm'},
+    ]},
+    {day:'friday',activities:[
+        {activity:'strength',instructor:'rochelle',time:'05:00am'},
+        {activity:'pilates',instructor:'stephanie',time:'08:15am'},
+        {activity:'yoga & flow',instructor:'mandy',time:'09:15am'},
+        {activity:'strength',instructor:'barbie',time:'10:15am'},
+        {activity:'strenght & flexibility',instructor:'stephanie',time:'11:30am'},
+        
+        {activity:'all-american dance',instructor:'amber',time:'04:30pm'},
+    ]},
 ]
 
 export const ACTIVITIES =['strength','pilates','weights','kickboxing','strength & flexibility','all-ages endurance','yoga & flow','all-american dance','ultimate endurance','skinny fit','fit & flex','fitness mix']
+
+export const FOOTER_LINKS=[
+    {title:'home',link:''},
+    {title:'contact',link:''},
+    {title:'admin login',link:''},
+    {title:'privacy policy',link:''},
+]
