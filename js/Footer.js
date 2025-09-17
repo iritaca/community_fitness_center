1073bd35e219898ab22c627600f18fb3cf520d5c
Isaac Rios <jirb1213@live.com.mx>
2025-09-17 13:49:17 -0600

    footer: visual repesentation

--- js/Footer.js
+++ js/Footer.js
@@ -1,29 +1,38 @@
 import { ValidateWrapper } from './utils.js';
 
-// @TODO: Add styles and documentation
+/** Footer
+ * -------
+ * Renders a footer navigation with links
+ * {only for presentation}
+ * @param {Object} props
+ * @param {Array} - Linsk to display
+ */
 export class Footer{
     constructor({links=[]}=[]){
         this.links = links
         this.wrapper = new ValidateWrapper({wrapper:'footer',componentName:this.constructor.name}).getWrapper()
         this.render()
     }
+    // Creates a <li><a> element for footer link
+    createLinkElement(title){
+        const li = document.createElement('li')
+            li.classList.add('footer-list-item')
+            const a = document.createElement('a')
+            a.setAttribute('href','#')
+            a.textContent = title
+            a.addEventListener('click',e=>e.preventDefault())
+            li.appendChild(a)
+            return li
+    }
+
     render(){
-        const frag = document.createDocumentFragment()
         const nav = document.createElement('nav')
         const ul = document.createElement('ul')
         ul.classList.add('footer-list')
-        for(const {title,link} of this.links){
-            const li = document.createElement('li')
-            li.classList.add('footer-list-item')
-            const a = document.createElement('a')
-            a.setAttribute('href',link)
-            a.textContent = title
-            li.appendChild(a)
-            ul.appendChild(li)
+        for(const {title} of this.links){
+            ul.appendChild(this.createLinkElement(title))
         }
-
         nav.appendChild(ul)
-        frag.appendChild(nav)
-        this.wrapper.appendChild(frag)
+        this.wrapper.appendChild(nav)
     }
 }
