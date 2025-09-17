733ccdb7d7a81b39c07f2b78218ac19c41f6837d
Isaac Rios <jirb1213@live.com.mx>
2025-09-17 13:01:50 -0600

    Observable documentation

--- js/Observable.js
+++ js/Observable.js
@@ -1,13 +1,27 @@
+/**
+ * Simple Observable
+ * Allows other code to subscribe to events and be notified when data changes
+ */
 export class Observable{
     constructor(){
+        /**
+         * @type {Function[]} - list of subscriber callback functions
+         */
         this.observers=[]
     }
 
-    // registers the observers
-    suscribe(fn){
-        this.observers.push(fn)
+    /**
+     * Rgisters a new observer callback
+     * @param {Function} callback - callback function to call on notify
+     */
+    subscribe(callback){
+        this.observers.push(callback)
     }
 
+    /**
+     * Notify all subscribers with given data
+     * @param {*} data - any data to send to observers
+     */
     notify(data){
         this.observers.forEach(fn=>fn(data))
     }
